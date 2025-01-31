b03b836758348613f3ae2cd7f326adb0bb204c28
masdefry <muhammaddefryan@gmail.com>
2025-01-31 08:50:33 +0700

    day05 done

---
+++ nested.loop.js
@@ -0,0 +1,16 @@
+for(let i=1; i<=5; i++){
+    console.log(`i = ${i}`)
+    for(let j=1; j<=5; j++){
+        console.log(`j = ${j}`)
+    }
+}
+
+for(let i=1; i<=5; i++){
+    console.log(`i = ${i}`)
+    for(let j=1; j<=5; j++){
+        console.log(`j = ${j}`)
+        for(let k=1; k<=5; k++){
+            console.log(`k = ${k}`)
+        }
+    }
+}
